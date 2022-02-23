e2acc145c3107bab4d379f17167a171a65ed5a7f
SalimmReza <refath300@gmail.com>
2022-02-23 23:15:54 +0600

    if a user is logged in one time he wont be logged out with out logging out

--- app/src/main/java/com/example/chattingappk/MainActivity.kt
+++ app/src/main/java/com/example/chattingappk/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.chattingappk
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,6 +10,9 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
+import android.content.SharedPreferences
+import android.content.SharedPreferences.Editor
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,11 +21,15 @@ class MainActivity : AppCompatActivity() {
     private lateinit var adapter: UsersAdapter
     private lateinit var auth:FirebaseAuth
     private  lateinit var  dbref: DatabaseReference
+    var sharedPreferences: SharedPreferences? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sharedPreferences = getSharedPreferences("autoLogin", Context.MODE_PRIVATE);
+
 
         auth= FirebaseAuth.getInstance()
         dbref=FirebaseDatabase.getInstance().getReference()
@@ -67,11 +75,20 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId==R.id.logout)
         {
-                auth.signOut()
+           /*     auth.signOut()
             val intent =Intent(this@MainActivity, LoginActivity::class.java)
             finish()
             startActivity(intent)
-            return true
+            return true*/
+
+
+            val editor = sharedPreferences!!.edit()
+            editor.putInt("key", 0)
+            editor.apply()
+
+            val intent = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(intent)
+
         }
 
         return true
